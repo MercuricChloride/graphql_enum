689c221b01e442eae6cba23510dcd5b4f376c31a
mercuricchloride <mercuryprotocol@protonmail.com>
2023-09-30 21:10:29 -0700

    wee

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,13 @@ pub fn graphql_to_enum(input: TokenStream) -> TokenStream {
     TokenStream::from(generated_code)
 }
 
+#[proc_macro]
+pub fn helpers_from_graphql(input: TokenStream) -> TokenStream {
+    let input_str = parse_macro_input!(input as LitStr).value();
+    let generated_code = generate_helper_functions(&input_str);
+    TokenStream::from(generated_code)
+}
+
 // Define the data structures
 #[derive(Debug, Clone)]
 struct Field {
@@ -115,20 +122,163 @@ fn upper_camel_case(input: &str) -> String {
     format!("{}{}", first_char, &input[1..])
 }
 
+fn camel_to_snake_case(input: &str) -> String {
+    let mut result = String::new();
+    for (i, c) in input.chars().enumerate() {
+        if c.is_uppercase() {
+            if i != 0 {
+                result.push('_');
+            }
+            result.push(c.to_lowercase().next().unwrap());
+        } else {
+            result.push(c);
+        }
+    }
+    result
+}
+
+#[test]
+fn test_camel_to_snake() {
+    assert_eq!(camel_to_snake_case("camelCase"), "camel_case");
+    assert_eq!(camel_to_snake_case("CamelCase"), "camel_case");
+    assert_eq!(camel_to_snake_case("camelcase"), "camelcase");
+    assert_eq!(camel_to_snake_case("camel_case"), "camel_case");
+    assert_eq!(camel_to_snake_case("Camel_Case"), "camel__case");
+}
+
+fn field_type_to_ident(field_type: &str) -> Ident {
+    match field_type {
+        "Int" => format_ident!("i32"),
+        "Float" => format_ident!("f32"),
+        "Boolean" => format_ident!("bool"),
+        _ => format_ident!("String"),
+    }
+}
+
+fn generate_create_params(fields: &Vec<Field>) -> proc_macro2::TokenStream {
+    let field_types: Vec<proc_macro2::TokenStream> = fields
+        .iter()
+        .filter_map(|field| {
+            if field.type_name == "ID" || !field.required || field.derived_from {
+                None
+            } else {
+                let field_type = field_type_to_ident(&field.type_name);
+                let snake_name = camel_to_snake_case(&field.name);
+                let field_name = format_ident!("{}", snake_name);
+                Some(quote!(#field_name: &#field_type))
+            }
+        })
+        .collect();
+
+    quote! {
+        #(#field_types),*
+    }
+}
+
+fn generate_set_statement(field: &Field) -> proc_macro2::TokenStream {
+    let key = format!("{}", &field.name);
+    let value = format_ident!("{}", camel_to_snake_case(&field.name));
+    match field.type_name.as_str() {
+        "BigInt" => {
+            quote!(
+                row.set_bigint(#key, #value);
+            )
+        }
+        _ => {
+            quote!(
+                row.set(#key, #value);
+            )
+        }
+    }
+}
+
+fn generate_create_set_statements(fields: &Vec<Field>) -> proc_macro2::TokenStream {
+    let set_statements: Vec<proc_macro2::TokenStream> = fields
+        .iter()
+        .filter_map(|field| {
+            if field.type_name == "ID" || !field.required || field.derived_from {
+                None
+            } else {
+                Some(generate_set_statement(&field))
+            }
+        })
+        .collect();
+
+    quote! {
+        #(#set_statements)*
+    }
+}
+
+fn generate_update_functions(table_name: &String, fields: &Vec<Field>) -> proc_macro2::TokenStream {
+    let update_functions: Vec<proc_macro2::TokenStream> = fields
+        .iter()
+        .filter_map(|field| {
+            if field.type_name == "ID" || field.derived_from {
+                None
+            } else {
+                let field_type = field_type_to_ident(&field.type_name);
+                let table_name = format!("{}", table_name);
+                let snake_name = camel_to_snake_case(&field.name);
+                let argument_name = format_ident!("{}", snake_name);
+                let update_function_name = format_ident!("update_{}", snake_name);
+
+                let set_statement = generate_set_statement(field);
+
+                Some(quote!(
+                    pub fn #update_function_name(tables: &mut Tables, id: &String, #argument_name: &#field_type) {
+                        let row = tables.update_row(#table_name, id);
+                        #set_statement
+                    }
+                ))
+            }
+        })
+        .collect();
+
+    quote! {
+        #(#update_functions)*
+    }
+}
+
+fn generate_helper_functions(input: &str) -> proc_macro2::TokenStream {
+    let (_, type_def) = type_def(input).unwrap();
+    let TypeDef { name, fields } = type_def;
+
+    let unit_struct_name = format_ident!("{}", &name);
+    let unit_struct = quote!(
+        pub struct #unit_struct_name;
+    );
+
+    let table_name = format!("{}", &name);
+    let create_function_params = generate_create_params(&fields);
+    let create_function_set_statements = generate_create_set_statements(&fields);
+    let create_function = quote!(
+        pub fn create(tables: &mut Tables, id: &'static str, #create_function_params) {
+            let row = tables.create_row(#table_name, id);
+            #create_function_set_statements
+        }
+    );
+
+    let update_functions = generate_update_functions(&table_name, &fields);
+
+    let impl_block = quote!(
+        impl #unit_struct_name {
+
+            #create_function
+
+            #update_functions
+        }
+    );
+
+    quote!(
+        #unit_struct
+        #impl_block
+    )
+}
+
 fn generate_enum_code(definition: &str) -> proc_macro2::TokenStream {
     let (_, type_def) = type_def(definition).unwrap();
 
     let TypeDef { name, fields } = type_def;
-
-    let fields = fields
-        .into_iter()
-        .map(|field| Field {
-            name: upper_camel_case(&field.name),
-            type_name: field.type_name,
-            required: field.required,
-            derived_from: field.derived_from,
-        })
-        .collect::<Vec<Field>>();
 
     let enum_name = format_ident!("{}", &name);
 
@@ -139,7 +289,7 @@ fn generate_enum_code(definition: &str) -> proc_macro2::TokenStream {
             if field.derived_from {
                 None
             } else {
-                let upper_camel_case = format_ident!("{}", &field.name);
+                let upper_camel_case = format_ident!("{}", upper_camel_case(&field.name));
                 Some(quote!(#upper_camel_case,))
             }
         })
@@ -152,15 +302,31 @@ fn generate_enum_code(definition: &str) -> proc_macro2::TokenStream {
     );
 
     let field_type_match: Vec<proc_macro2::TokenStream> = fields
+        .clone()
         .into_iter()
         .filter_map(|field| {
             if field.derived_from {
                 None
             } else {
-                let field_type = format_ident!("{}", &field.name);
+                let field_type = format_ident!("{}", upper_camel_case(&field.name));
                 let field_type_string = format!("{}", &field.type_name);
                 Some(quote!(
                     #enum_name::#field_type => #field_type_string,
+                ))
+            }
+        })
+        .collect();
+
+    let field_key_match: Vec<proc_macro2::TokenStream> = fields
+        .into_iter()
+        .filter_map(|field| {
+            if field.derived_from {
+                None
+            } else {
+                let field_type = format_ident!("{}", upper_camel_case(&field.name));
+                let field_name_string = format!("{}", &field.name);
+                Some(quote!(
+                    #enum_name::#field_type => #field_name_string,
                 ))
             }
         })
@@ -173,6 +339,16 @@ fn generate_enum_code(definition: &str) -> proc_macro2::TokenStream {
                     #(#field_type_match)*
                 }
             }
+
+            pub fn key(&self) -> &'static str {
+                match self {
+                    #(#field_key_match)*
+                }
+            }
+
+            //pub fn create_entry(tables: &mut Tables, id: &'static str, #generate_required_fields_params(&fields)) {
+                //tables.create_row(#enum_name::Table.key(), id)
+            //}
         }
     );
 
@@ -191,8 +367,8 @@ type ProtocolCollateral @entity {
   derivedShit: Fart! @derivedFrom(sdflkasdflkjasdflk)
 }"#;
 
-    println!(
-        "Graphql to rust enum\n\n\n\n{}",
-        parse_graphql_to_rust_enum(input.trim())
-    );
+    // println!(
+    //     "Graphql to rust enum\n\n\n\n{}",
+    //     parse_graphql_to_rust_enum(input.trim())
+    // );
 }
